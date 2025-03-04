19dbbe0229d277461457667db001051227ed2dca
Leo <leonsiypopov@gmail.com>
2025-03-04 19:14:21 +0200

    done

--- taxi/urls.py
+++ taxi/urls.py
@@ -16,15 +16,12 @@ urlpatterns = [
         name="manufacturers"
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/", CarListView.as_view(), name="cars"),
-
     path(
         "cars/<int:pk>/",
         CarDetailView.as_view(),
         name="car-detail",
     ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/", DriverListView.as_view(), name="drivers"),
     path(
         "drivers/<int:pk>/",
         DriverDetailView.as_view(),
